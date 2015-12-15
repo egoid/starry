df972fe28039c96ef95d422897e95aff9444bc2c
Ta <ta@Tas-MacBook-Pro.local>
2015-12-15 12:44:18 -0800

    profile add

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 var starry = angular.module('starter', ['gajus.swing' , 'ionic', 'firebase', 'ngStorage', 'ngCordova']);
 var fb = null;
 
-
 starry.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
         if(window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if(window.StatusBar) {
             StatusBar.styleDefault();
         }
-        fb = new Firebase("https://starry-starry.firebaseio.com/");
+        fb = new Firebase("https://larpameet.firebaseio.com/");
     });
 });
 
@@ -41,65 +38,28 @@ starry.config(function($stateProvider, $urlRouterProvider) {
         });
     $urlRouterProvider.otherwise('/splash');
 });
-starry.controller("LoginController", function($scope, $cordovaOauth, $localStorage, $location, $ionicModal, $firebaseAuth, $location) {
-
-    $scope.login = function() {
-        $cordovaOauth.facebook("434572043406554", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
-            localStorage.accessToken = result.access_token;
-            $location.path("/profile");
-        }, function(error) {
-            $location.path("/profile");
-            console.log(error);
-        })
-
-    }
-
-
-    $ionicModal.fromTemplateUrl('my-modal.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then(function(modal) {
-      $scope.modal = modal;
-    });
-    $scope.openModal = function() {
-      $scope.modal.show();
-    };
-    $scope.closeModal = function() {
-      $scope.modal.hide();
-    };
-    //Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.modal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-      // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-    })
-
-    $scope.register = function(email, password) {
-        var fbAuth = $firebaseAuth(fb);
-        fbAuth.$createUser({ email: email, password: password }).then(function() {
-            return fbAuth.$authWithPassword({
-                email: email,
-                password: password
-            })
-        }).then(function(authData) {
-            $location.path("/profile");
-        }).catch(function(error) {
-            console.error("ERROR " + error);
-        });
-    } 
-});
 
 starry.controller("ProfileController", function($scope, $http, $localStorage, $location) {
-
+    $scope.picture = localStorage.picture;
     $scope.init = function() {
         if(localStorage.hasOwnProperty("accessToken") === true) {
             $http.get("https://graph.facebook.com/v2.5/me", { params: { access_token: localStorage.accessToken, fields: "id,name,gender,location,picture,relationship_status", format: "json" }}).then(function(result) {
+                fb.on("value", function(snapshot) {
+                    if (snapshot.child(result.data.id).exists() === false) {
+                        fb.set({
+                            [result.data.id]: {
+                                full_name: result.data.name ,
+                                location: result.data.location.name,
+                                picture: result.data.picture.data.url,
+                                gender: result.data.gender,
+                                photos: [],
+                                bio: "",
+                                description: [],
+                                matches: {},
+                            }
+                        })
+                    }
+                })
                 $scope.profileData = result.data;
             }, function(error) {
                 alert("There was a problem getting your profile.  Check the logs for details.");
@@ -112,36 +72,12 @@ starry.controller("ProfileController", function($scope, $http, $localStorage, $l
             });
         } else {
             console.log("Not signed in");
-        }
+        };
 
 
     };
 
 });
-starry.controller("FeedController", function ($scope) {
-    var stack;
-
-    stack = gajus.Swing.Stack();
-
-    [].forEach.call(document.querySelectorAll('.stack li'), function (targetElement) {
-        stack.createCard(targetElement);
-
-        targetElement.classList.add('in-deck');
-    });
-
-    stack.on('throwout', function (e) {
-        console.log(e.target.innerText || e.target.textContent, 'has been thrown out of the stack to the', e.throwDirection == 1 ? 'right' : 'left', 'direction.');
-
-        e.target.classList.remove('in-deck');
-    });
-
-    stack.on('throwin', function (e) {
-        console.log(e.target.innerText || e.target.textContent, 'has been thrown into the stack from the', e.throwDirection == 1 ? 'right' : 'left', 'direction.');
-
-        e.target.classList.add('in-deck');
-    });
-
-})
 starry.controller("SplashController", function ($scope, $cordovaOauth, $localStorage, $location, $ionicModal, $firebaseAuth, $location) {
     $scope.init = function() {
           window.fbAsyncInit = function() {
@@ -189,6 +125,9 @@ starry.controller("SplashController", function ($scope, $cordovaOauth, $localSto
                     console.log('has the win!')
                 }
             });
+            FB.api('/me/picture', {type: "large"}, function(response) {
+                localStorage.picture = response.data.url;
+            })
             FB.api('/me', function(response) {
               console.log('Successful login for: ' + response.name);
               document.getElementById('status').innerHTML =
@@ -202,8 +141,6 @@ starry.controller("SplashController", function ($scope, $cordovaOauth, $localSto
             js.src = "//connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
-
-
         $.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyC5kjfXM3a3lt5QryC5gCA_ejK3-Ve89_0', function(data, status) {
             console.log(data)
             $localStorage.geo = data;
@@ -233,8 +170,8 @@ starry.controller("SplashController", function ($scope, $cordovaOauth, $localSto
         e.target.classList.add('in-deck');
          $scope.login();
     });
-    $scope.login = function() {
-    $cordovaOauth.facebook("434572043406554", ["email", "user_website", "user_location", "user_relationships", "user_photos"]).then(function(result) {
+    $scope.login = function() { 
+    $cordovaOauth.facebook("434572043406554", ["email", "user_website", "user_location"]).then(function(result) {
         $localStorage.accessToken = result.access_token;
         $location.path("/profile");
     }, function(error) {
